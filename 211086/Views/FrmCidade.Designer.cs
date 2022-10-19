b4f4f06eed0f2cfeac2d39ba4c527028a5bd616d
VictorVendrameto <vendrametovictor@gmail.com>
2022-10-19 11:07:03 -0300

    segundo commit

---
+++ 211086/Views/FrmCidade.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace _211086.Views
+{
+    partial class FrmCidade
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCidades = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAlternar = new System.Windows.Forms.Button();
+            this.btnIncluir = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUF = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCidades)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCidades
+            // 
+            this.dgvCidades.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCidades.Location = new System.Drawing.Point(20, 282);
+            this.dgvCidades.Name = "dgvCidades";
+            this.dgvCidades.Size = new System.Drawing.Size(635, 150);
+            this.dgvCidades.TabIndex = 30;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(193, 13);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Digite o nome da Cidade para Pesquisa";
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPesquisar.Location = new System.Drawing.Point(670, 225);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(102, 61);
+            this.btnPesquisar.TabIndex = 28;
+            this.btnPesquisar.Text = "🔎";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 256);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(635, 20);
+            this.textBox1.TabIndex = 27;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Location = new System.Drawing.Point(636, 161);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(148, 48);
+            this.btnFechar.TabIndex = 26;
+            this.btnFechar.Text = "🚪Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.Location = new System.Drawing.Point(482, 161);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(148, 48);
+            this.btnExcluir.TabIndex = 25;
+            this.btnExcluir.Text = "🗑Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(328, 161);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(148, 48);
+            this.btnCancelar.TabIndex = 24;
+            this.btnCancelar.Text = "✖Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnAlternar
+            // 
+            this.btnAlternar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlternar.Location = new System.Drawing.Point(174, 161);
+            this.btnAlternar.Name = "btnAlternar";
+            this.btnAlternar.Size = new System.Drawing.Size(148, 48);
+            this.btnAlternar.TabIndex = 23;
+            this.btnAlternar.Text = "🔁Alterar";
+            this.btnAlternar.UseVisualStyleBackColor = true;
+            // 
+            // btnIncluir
+            // 
+            this.btnIncluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIncluir.Location = new System.Drawing.Point(20, 161);
+            this.btnIncluir.Name = "btnIncluir";
+            this.btnIncluir.Size = new System.Drawing.Size(148, 48);
+            this.btnIncluir.TabIndex = 22;
+            this.btnIncluir.Text = "➕Incluir ";
+            this.btnIncluir.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(667, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "UF";
+            // 
+            // txtUF
+            // 
+            this.txtUF.Location = new System.Drawing.Point(670, 103);
+            this.txtUF.Name = "txtUF";
+            this.txtUF.Size = new System.Drawing.Size(77, 20);
+            this.txtUF.TabIndex = 20;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Cidade";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(20, 103);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(619, 20);
+            this.txtNome.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Código";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(20, 35);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 16;
+            // 
+            // FrmCidade
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.dgvCidades);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAlternar);
+            this.Controls.Add(this.btnIncluir);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUF);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtID);
+            this.Name = "FrmCidade";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Cidades";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCidades)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCidades;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAlternar;
+        private System.Windows.Forms.Button btnIncluir;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUF;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtID;
+    }
+}
--- 211086/frmMenu.Designer.cs
+++ 211086/frmMenu.Designer.cs
@@ -56,7 +56,7 @@
             this.cadastrosToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(813, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(993, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -211,7 +211,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(813, 450);
+            this.ClientSize = new System.Drawing.Size(993, 450);
             this.Controls.Add(this.dgvCidades);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btnPesquisar);
